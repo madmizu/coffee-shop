297432780ff6dd863df4f45cac5647748546d781
Madison Shimizu <mhshimizu@outlook.com>
2021-12-21 09:15:47 -1000

    submit

--- app/model/Coffee.rb
+++ app/model/Coffee.rb
@@ -3,9 +3,12 @@ class Coffee < ActiveRecord::Base
     has_many :customers, through: :orders
 
     def unique_customer_names
+        self.customers.map{|c| c[:name]}
     end
 
     def self.most_ordered
+        binding.pry
+        
     end
 end
   
